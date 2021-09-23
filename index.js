d5b8c169579d1e7116f0faf3df4bc0aa2ef08d8a
sid1-3 <88434832+sid1-3@users.noreply.github.com>
2021-09-23 15:53:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+for (var i = 0; i < 7; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+});
+}
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+});
+
+  function makeSound(key){
+    switch (key) {
+      case 'w':
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+      case 'a':
+        var kickBass = new Audio('sounds/kick-bass.mp3');
+        kickBass.play();
+        break;
+
+      case 's':
+        var crash = new Audio('sounds/snare.mp3');
+        crash.play();
+        break;
+
+      case 'd':
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+
+      case 'j':
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+
+      case 'k':
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+
+      case 'l':
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+
+      default: console.log(buttonInnerHTML);
+    }
+  }
